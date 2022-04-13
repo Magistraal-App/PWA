595450ca8baab6c95135bdcbc8cd2b466ec98073
Tjalling Feenstra <webmaster@tjalling.net>
2022-04-13 17:04:30 +0200

    PWA bijgewerkt

--- client/main/index.php
+++ client/main/index.php
@@ -7,9 +7,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#202124">
+    <meta name="theme-color" content="#000000">
 
-    <link rel="manifest" href="../../manifest.json" />
+    <link rel="manifest" href="/magistraal/manifest.json" />
+    <link rel="icon" type="image/ico" href="/magistraal/client/favicon.ico">
 
     <?php echo(\Magistraal\Frontend\assetsHTML()); ?>
 
