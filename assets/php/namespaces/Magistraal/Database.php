9c2ec5b0e21c05b3e784cdc47ffb6327376c6264
Tjalling Feenstra <webmaster@tjalling.net>
2022-05-13 06:38:55 +0200

    Query method toegevoegd

--- assets/php/namespaces/Magistraal/Database.php
+++ assets/php/namespaces/Magistraal/Database.php
@@ -2,12 +2,56 @@
     namespace Magistraal\Database;
 
     function connect() {       
-        return new \MysqliDb(
+        return new \mysqli(
             \Magistraal\Config\get('mysql_hostname'),
             \Magistraal\Encryption\decrypt(\Magistraal\Config\get('mysql_username')),
             \Magistraal\Encryption\decrypt(\Magistraal\Config\get('mysql_password')),
-            \Magistraal\Config\get('mysql_database'),
-            !\Magistraal\Config\get('production')
+            \Magistraal\Config\get('mysql_database')
         );
+    }
+
+    function query(string $query, $parameters = []) {
+        if(!is_array($parameters)) {
+            $parameters = [$parameters];
+        }
+
+        $mysqli = \Magistraal\Database\connect();
+
+        $parameter_types_string = '';
+        foreach ($parameters as $parameter) {
+            if(is_string($parameter)) {
+                $parameter_types_string .= 's';
+            } else if(is_integer($parameter)) {
+                $parameter_types_string .= 'i';
+            } else if(is_float($parameter)) {
+                $parameter_types_string .= 'd';
+            } else {
+                $parameter_types_string .= 'b';
+            }
+        }
+
+        $stmt = $mysqli->prepare($query);
+
+        if(is_bool($stmt)) {
+            return false;
+        }
+        
+        if(count($parameters) > 0) {
+            $stmt->bind_param($parameter_types_string, ...$parameters);
+        }
+        
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if(is_bool($result)) {
+            return true;
+        }
+
+        $rows = [];
+        while ($row = $result->fetch_array(MYSQLI_BOTH)) {
+            $rows[] = $row;
+        }
+        
+        return $rows;
     }
 ?>
